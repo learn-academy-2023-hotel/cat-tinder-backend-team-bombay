07bc5ae5ccb764d15349e9ee3d947a68f8df74ce
EWinkeljohn <emcwink@gmail.com>
2023-12-07 14:43:23 -0800

    API validations complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,19 @@ cats = [
     {
         name: "Dan",
         age: 13,
-        enjoys: "eating",
+        enjoys: "eating wet food",
         image: 'https://as2.ftcdn.net/v2/jpg/03/03/62/45/1000_F_303624505_u0bFT1Rnoj8CMUSs8wMCwoKlnWlh5Jiq.jpg'
     },
     {
         name: "Nova",
         age: 2,
-        enjoys: "sleeping",
+        enjoys: "sleeping all day",
         image: 'https://as2.ftcdn.net/v2/jpg/00/77/51/81/1000_F_77518136_F88I0v3R2mZsKEgxxXMc4iqXlOjK8OLE.jpg'
     },
     {
         name: "Stevie",
         age: 3,
-        enjoys: "hiding",
+        enjoys: "hiding under the couch",
         image: 'https://th-thumbnailer.cdn-si-edu.com/bgmkh2ypz03IkiRR50I-UMaqUQc=/1000x750/filters:no_upscale():focal(1061x707:1062x708)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/55/95/55958815-3a8a-4032-ac7a-ff8c8ec8898a/gettyimages-1067956982.jpg'
     }
 ]
--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate name" do
+    cat = Cat.create(age: 3, enjoys: 'hiding under the couch', image: 'https://th-thumbnailer.cdn-si-edu.com/bgmkh2ypz03IkiRR50I-UMaqUQc=/1000x750/filters:no_upscale():focal(1061x707:1062x708)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/55/95/55958815-3a8a-4032-ac7a-ff8c8ec8898a/gettyimages-1067956982.jpg')
+    expect(cat.errors[:name]).to_not be_empty
+  end
+  it "should validate age" do
+    cat = Cat.create(name: 'Stevie', enjoys: 'hiding under the couch', image: 'https://th-thumbnailer.cdn-si-edu.com/bgmkh2ypz03IkiRR50I-UMaqUQc=/1000x750/filters:no_upscale():focal(1061x707:1062x708)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/55/95/55958815-3a8a-4032-ac7a-ff8c8ec8898a/gettyimages-1067956982.jpg')
+    expect(cat.errors[:age]).to_not be_empty
+  end
+  it "should validate enjoys" do
+    cat = Cat.create(name: 'Stevie', age: 3, image: 'https://th-thumbnailer.cdn-si-edu.com/bgmkh2ypz03IkiRR50I-UMaqUQc=/1000x750/filters:no_upscale():focal(1061x707:1062x708)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/55/95/55958815-3a8a-4032-ac7a-ff8c8ec8898a/gettyimages-1067956982.jpg')
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+  it "should validate image" do
+    cat = Cat.create(name: 'Stevie', age: 3, enjoys: 'hiding under the couch')
+    expect(cat.errors[:image]).to_not be_empty
+  end
+  it "validates enjoys is at least 10 characters" do
+    cat = Cat.create(name: 'Stevie', age: 3, enjoys: 'hiding', image: 'https://th-thumbnailer.cdn-si-edu.com/bgmkh2ypz03IkiRR50I-UMaqUQc=/1000x750/filters:no_upscale():focal(1061x707:1062x708)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/55/95/55958815-3a8a-4032-ac7a-ff8c8ec8898a/gettyimages-1067956982.jpg')
+    expect(cat.enjoys).to_not be_empty
+  end 
 end
